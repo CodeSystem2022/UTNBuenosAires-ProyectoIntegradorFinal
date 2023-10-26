d59a452f71b21ccf6140b4ea72a721753d253853
piaolivera <piaoliveraa@gmail.com>
2023-10-26 00:15:09 -0300

    CONEXIÓN A MERCADO PAGO

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -200,3 +200,96 @@ function actualizarNumerito() {
     numerito.innerText = nuevoNumerito;
 }
 
+
+
+
+
+// MERCADO PAGO
+
+
+
+const mercadopago = new MercadoPago("PUBLIC_KEY", {
+    locale: '<LOCALE>' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
+  });
+  
+  // Handle call to backend and generate preference.
+  document.getElementById("carrito-acciones-comprar").addEventListener("click", function () {
+  
+    $('#checkout-btn').attr("disabled", true);
+  
+    const orderData = {
+      quantity: document.getElementById("quantity").value,
+      description: document.getElementById("product-description").innerHTML,
+      price: document.getElementById("unit-price").innerHTML
+    };
+  
+    fetch("http://localhost:8080/create_preference", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderData),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (preference) {
+        createCheckoutButton(preference.id);
+  
+        $(".shopping-cart").fadeOut(500);
+        setTimeout(() => {
+          $(".container_payment").show(500).fadeIn();
+        }, 500);
+      })
+      .catch(function () {
+        alert("Unexpected error");
+        $('#checkout-btn').attr("disabled", false);
+      });
+  });
+  
+  function createCheckoutButton(preferenceId) {
+    // Initialize the checkout
+    const bricksBuilder = mercadopago.bricks();
+  
+    const renderComponent = async (bricksBuilder) => {
+      if (window.carrito-acciones-comprar) window.carrito-acciones-comprar.unmount();
+      await bricksBuilder.create(
+        'wallet',
+        'carrito-acciones-comprar', // class/id where the payment button will be displayed
+        {
+          initialization: {
+            preferenceId: preferenceId
+          },
+          callbacks: {
+            onError: (error) => console.error(error),
+            onReady: () => {}
+          }
+        }
+      );
+    };
+    window.checkoutButton =  renderComponent(bricksBuilder);
+  }
+  
+  // Handle price update
+  function updatePrice() {
+    let quantity = document.getElementById("quantity").value;
+    let unitPrice = document.getElementById("unit-price").innerHTML;
+    let amount = parseInt(unitPrice) * parseInt(quantity);
+  
+    document.getElementById("cart-total").innerHTML = "$ " + amount;
+    document.getElementById("summary-price").innerHTML = "$ " + unitPrice;
+    document.getElementById("summary-quantity").innerHTML = quantity;
+    document.getElementById("summary-total").innerHTML = "$ " + amount;
+  }
+  
+  document.getElementById("quantity").addEventListener("change", updatePrice);
+  updatePrice();
+  
+  // Go back
+  document.getElementById("go-back").addEventListener("click", function () {
+    $(".container_payment").fadeOut(500);
+    setTimeout(() => {
+      $(".shopping-cart").show(500).fadeIn();
+    }, 500);
+    $('#checkout-btn').attr("disabled", false);
+  });
