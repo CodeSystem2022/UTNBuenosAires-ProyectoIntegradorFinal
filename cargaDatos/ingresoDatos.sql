7d0b4e3aefa31d0c6d14f5924487c7419d7cc936
Viviana A. Montenegro <vivy.montenegro@gmail.com>
2023-11-07 18:17:42 -0300

    Carga inicial de tablas

---
+++ cargaDatos/ingresoDatos.sql
@@ -0,0 +1,30 @@
+
+--Insercion datos tabla usuarios
+insert into usuario (nombre,email,direccion,telefono,password)
+values ('Juan Lopez','jlopez@email.com','Alsina 123 Rosario','114323456','jlopez'),
+       ('Maria Garcia','mgarcia@email.com','Juncal 83 Cordoba ','1152632563','mgarcia'),
+       ('Mario Rios','mrios@email.com','Rivadavia 1400 CAp ','1145891452','mrios')
+
+
+
+--Insercion datos tabla productos
+
+insert into productos (marca,descripcion,precio,cantidad,imagen,categoria)
+
+values ('Asus','X515EA Corei3 12GB 480Gb',552000,10,' ','N'),
+       ('HP','X360 Pentium 4GB Ssd 256GB ',389000,17,' ','N'),
+       ('Lenovo','Yoga Slim7 Ryzen 5 8GB Ssd',901000,5,' ','N'),
+       ('Acer','Aspire 3 Core i3 8gb Ssd 256Gb',517000,9,' ','N'),
+       ('LG','Hd 60 Hz 5Ms 19M38A',90400,21,' ','M'),
+       ('Viewsonic','60Hz Vga Dvi VA2055SM',83900,26,' ','M'),
+       ('Samsung','Led 22" full Hd 75Hz',86900,15,' ','M'),
+       ('Noblex','Full Hd 75Hz Vga Hdmi',161480,9,' ','M'),
+       ('Logitech','Mouse M170',10499,8,' ','A'),
+       ('Redragon','Camara WEb GW600',25000,25,'','A'),
+       ('Logitech','Teclado K480 inalambrico',40800,30,' ', 'A'),
+       ('Gaming','Auricular Gamer JBL',206000,9,' ','A'),
+       ('Netac','Memoria Ram DDr4',18349,15,' ','O'),
+       ('Xtrike','Silla Gamer ',960000,2,' ','O'),
+       ('Lyonn','Ups CTB-3000 VA',250000,3,' ','O')
+
+
